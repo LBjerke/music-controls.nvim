b71452e1e5edfa6330b3605d69e0c2b5bbe07891
Loyde Cochran-Bjerke <Loyde.b.bjerke@gmail.com>
2025-02-02 15:59:26 -0600

    Update music-controls.lua

--- plugin/music-controls.lua
+++ plugin/music-controls.lua
@@ -2,10 +2,6 @@ if vim.g.loaded_music_controls then
   return
 end
 
-if vim.fn.executable('playerctl') == 0 then
-  vim.api.nvim_err_writeln('MusicControls: playerctl is not installed')
-  return
-end
 
 require('music-controls').setup()
 require('music-controls.commands').setup()
